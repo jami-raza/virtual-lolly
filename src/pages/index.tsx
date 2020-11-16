bcb5b2f6cdfad51e2914bba28d92391d351dc8ad
jami raza <muhammadjmai45@gmail.com>
2020-11-16 17:55:02 +0500

    initial stage

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Lolly from '../Components/lolly'
+export default function(){
+    return(
+        <div>
+            Start the lolly creation
+            <Lolly Top={"yellow"} Bottom={"black"} Middle={"blue"}/>
+        </div>
+    )
+}
