de7ed3a216dce94c0804b9f2426dddf524014c1c
jami raza <muhammadjmai45@gmail.com>
2020-11-17 19:32:24 +0500

    update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,6 @@
 module.exports = {
     plugins: [
         
-        {
-            resolve: "gatsby-source-graphql",
-            options: {
-              // Arbitrary name for the remote schema Query type
-              typeName: "SWAPI",
-              // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
-              fieldName: "swapi",
-              // Url to query from
-              url: "http://localhost:8888/.netlify/functions/Vcard",
-            },
-        }
+        
     ]
 }
